7d38952b9eba9e1321ab470a3deb1bb1aa9abbde
Mistire <mdiwithgod@gmail.com>
2025-07-02 11:44:55 +0300

    Add correlated and non-correlated subqueries for user bookings and property ratings

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,18 +1,18 @@
 -- Write a query to find all properties where the average rating is greater than 4.0 using a subquery.
--- SELECT
---   p.property_id,
---   p.name,
---   p.created_at,
---   p.updated_at
--- FROM Property p 
--- JOIN (
---   SELECT
---     property_id,
---     AVG(rating) as avg_rating 
---   FROM Review
---   GROUP BY property_id
---   HAVING AVG(rating) > 4.0
--- ) as r_avg ON p.property_id = r_avg.property_id;
+SELECT
+  p.property_id,
+  p.name,
+  p.created_at,
+  p.updated_at
+FROM Property p 
+JOIN (
+  SELECT
+    property_id,
+    AVG(rating) as avg_rating 
+  FROM Review
+  GROUP BY property_id
+  HAVING AVG(rating) > 4.0
+) as r_avg ON p.property_id = r_avg.property_id;
 
 -- Write a correlated subquery to find users who have made more than 3 bookings.
 SELECT

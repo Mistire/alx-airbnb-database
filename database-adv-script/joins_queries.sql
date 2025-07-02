87899a9c490f2ea3af91f64a8982276ea4f489b3
Mistire <mdiwithgod@gmail.com>
2025-07-02 10:03:06 +0300

    fix: Update the full outer join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -17,21 +17,21 @@ SELECT
 FROM Property LEFT JOIN Review ON Property.property_id = Review.property_id
 ORDER BY Property.property_id
 
--- OUTER JOIN
+-- FULL OUTER JOIN
 SELECT 
   User.user_id,
   User.email,
   Booking.booking_id,
   Booking.start_date,
   Booking.end_date
-FROM User OUTER JOIN Booking ON User.user_id = Booking.user_id
+FROM User LEFT JOIN Booking ON User.user_id = Booking.user_id
 
--- UNION
+UNION
 
--- SELECT 
---   Booking.booking_id,
---   Booking.start_date, 
---   Booking.end_date,
---   User.user_id,
---   User.email
--- FROM Booking INNER JOIN User ON Booking.user_id = User.user_id;
+SELECT 
+  Booking.booking_id,
+  Booking.start_date, 
+  Booking.end_date,
+  User.user_id,
+  User.email
+FROM Booking RIGHT JOIN User ON Booking.user_id = User.user_id;
